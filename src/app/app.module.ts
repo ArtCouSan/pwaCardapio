4958b79051409026219fd9abc99bed37b21b9f90
ArtCouSan <artcousan@gmail.com>
2018-02-15 22:27:24 -0200

    Rotas tiradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Routes, RouterModule } from '@angular/router';
+
 import { FormsModule } from '@angular/forms';
 import { OrderModule } from 'ngx-order-pipe';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,15 +7,6 @@ import { FilterPipe } from './filter.pipe';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-
-const appRoutes: Routes = [
-  {
-    path: '', 
-    component: AppComponent
-  },
-  
-];
-
 
 @NgModule({
   declarations: [
@@ -26,11 +17,7 @@ const appRoutes: Routes = [
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     OrderModule,
-    FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
